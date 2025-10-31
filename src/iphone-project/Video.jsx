e502732a6a9daf2d5ac2ac49153bd0917b9e923a
DavidParis04 <davidonoja6@gmail.com>
2025-10-31 09:25:51 +0100

    Fixed fonts and sourcemap

--- src/iphone-project/Video.jsx
+++ src/iphone-project/Video.jsx
@@ -1,8 +1,8 @@
 import React from "react"
-import testimony from "./assets/review.mp4";
+import testimony from "./assets/review0.mp4";
 export function Video()
 {
-    return ( <div className="video-container" >
+    return ( <div className="video-container" style={{width:"100%", height:"100%"}} >
         <video className="video-itself" src={testimony} autoPlay loop controls ></video>
     </div>)
 }
